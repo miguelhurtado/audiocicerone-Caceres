dba3194451a50a4bb81c963dd5d2f5e69b7d15b5
Miguel Hurtado <miguel.hurtado@perianet.com>
2014-03-24 11:56:35 +0100

    comienzo final

---
+++ README.md
@@ -0,0 +1,14 @@
+audioguia-Cicerone
+==================
+
+APP Audio guía turística de Cáceres
+
+La AudioGuía Turística-Histórica de "Cáceres Ciudad Monumental" está recomendada para utilizar in-situ. Es una aplicación jquery para todo tipo de dispositivos que recoge información histórico-artística de los edificios más importantes de la ciudad monumental de Cáceres.  
+
+En esta audioguia ofrecemos treinta pistas de sonido y transcripciones con definiciones en glosarios de términos, ubicaciones, recomendaciones y propuestas de rutas. Todo el contenido con vistas a pie de calle con Street View de Google.
+
+A través de esta audioguia podrán seguir las características artísticas e históricas de estos edificios. Para ello se ha utilizado un lenguaje en donde algunas palabras técnicas artísticas han sido elegidas para que todos podamos tener más conocimientos sobre Historia del Arte.
+
+Las pistas pueden ser escuchadas infinitamente y están apoyadas por la edición de texto de cada una de ellas, al igual que un glosario de términos artísticos.
+
+Para cualquier sugerencia, pueden ponerse en contacto con nosotros a través del contacto que proporcionamos en la aplicación.
---
+++ js/resourceLoader.js
@@ -0,0 +1,240 @@
+/**
+ * Resource loader class - this class is responsible for loading
+ * all javascript / css resources.
+ * 
+ * @author Eran Zinman
+ */
+var ResourceLoader = (function() 
+{
+    var me = {};
+	
+	// Content to be written to document
+	var documentWrite = '';
+
+	/**
+	 * Load a given resource file, by document.write into the HTML.
+	 * 
+	 * @param {string} file - Relative path of the resource URL to include
+	 * @param {string} type - ('css', 'js') - The type of resource to load
+	 * @author Eran Zinman
+	 */
+	function loadResource(file, type)
+	{
+		if (type === 'js')
+		{
+			documentWrite += '<script type="text/javascript" src="' + file + '"></' + 'script>';
+		}
+		else if (type === 'css')
+		{
+			documentWrite += '<link type="text/css" rel="stylesheet" href="' + file + '">';
+		}	
+	}
+	
+	/**
+	 * Check if criteria is met or not
+	 * 
+	 * @param {Object} criteriaArray - criteria array to check
+	 * @return {Boolean} true if criteria is valid, false otherwise
+	 * @author Eran Zinman
+	 */
+	function isCriteriaMet(criteriaArray)
+	{
+		// If criteria is empty, we are good
+		if (criteriaArray.length == 0)
+		{
+			return true;
+		}
+		
+		// Loop through "yeah" array, and verify it's valid
+		for (var i = 0; i < criteriaArray.length; i++)
+		{
+			// Get current criteria item
+			var item = criteriaArray[i];
+			
+			// Split into item array
+			var itemArray = item.split('&');
+			
+			// Loop through item array
+			var isValid = true;
+			for (j = 0; j < itemArray.length; j++)
+			{
+				// Check if this criteria item equals to one of our platforms or device
+				var curCriteria = itemArray[j];
+				if ((typeof(platformEnum[curCriteria]) !== 'undefined' && platformEnum[curCriteria] != PLATFORM) ||
+					(typeof(deviceTypeEnum[curCriteria]) !== 'undefined' && deviceTypeEnum[curCriteria] != DEVICE))
+				{
+					// This is valid !
+					isValid = false;
+				}	
+			} 
+			
+			// If current item is valid, return true and break the main loop
+			if (isValid)
+			{
+				return true;
+			}
+		}
+		
+		// We are done, none of the criteria items was valid - criteria is not met
+		return false;
+	}
+
+	/**
+	 * Check if the given resource should be loaded or not (according
+	 * to the current device and platform)
+	 * 
+	 * @param {Object} resource - The resource object, containing an optional "yeah" and "nope" arrays
+	 * @return {boolean} true if we should load resource, false otherwise
+	 * @author Eran Zinman
+	 */
+	me.isValidResource = function(resource)
+	{	
+		// A resource is valid iff is "yeah" parameter is valid (or empty),
+		// and his "nope" parameter is not valid
+		var isYeah = true;
+		var isNope = false;		
+			        
+		// If resource is valid
+		if (resource) 
+		{	
+			// Get "yeah" and "nope" variables
+			var yeahCriteria = resource.yeah || [];
+			var nopeCriteria = resource.nope || [];
+			
+			// Get "yeah" and "nope" crteria's
+			isYeah = isCriteriaMet(yeahCriteria);
+			
+			if (nopeCriteria.length > 0) 
+			{
+				isNope = isCriteriaMet(nopeCriteria);
+			}
+		}
+	    
+	    // Check that both device and platform are OK
+		var isValid = isYeah && !isNope;
+	    return isValid;
+	}
+	
+	function getResourcesToLoad(resources, prefix)
+	{
+		var resourcesToLoad = [];
+		
+		// Check if we set the folder prefix
+		prefix = prefix || '';
+		
+		// Loop through resources
+		var minifiedResource = null;
+		for (minifiedResource in resources)
+		{
+			// Get resource and it's type
+			var resource = resources[minifiedResource];
+			var resourceType = resource.type;
+			
+			// Check if we are in DEBUG / RELEASE modes. Try to fetch
+			// this state from the parent (if executed from a page)
+			var debugMode = false;
+			try 
+			{
+				if (typeof window.DEBUG !== 'undefined') 
+				{
+					debugMode = window.DEBUG;
+				}
+				else if (typeof parent.DEBUG !== 'undefined') 
+				{
+					debugMode = parent.DEBUG;
+				}
+			} 
+			catch (e) 
+			{
+				// Prevent exception when unable to access parent
+			}
+			
+			// In release, load minified file
+			if (!debugMode)
+			{
+				// Add prefix
+				minifiedResource = prefix + minifiedResource;	
+				
+				// Add minified resource to array
+				resourcesToLoad.push({ file: minifiedResource, type: resourceType });
+			}			
+			// In debug, load each file individually
+			else
+			{
+				// Did we specify any files to load?
+				if (resource.files) 
+				{
+					// Loop through files to load
+					var files = resource.files;
+					for (var i = 0; i < files.length; i++) 
+					{
+						var resourceObj = files[i];
+						if (me.isValidResource(resourceObj)) 
+						{
+							// Add minified resource to array
+							var fileResource = prefix + resourceObj['file'];
+							resourcesToLoad.push({ file: fileResource, type: resourceType });
+						}
+					}
+				}
+			}
+		}
+		
+		return resourcesToLoad;
+	}
+	
+	/**
+	 * Loads the given resources array.
+	 * 
+	 * @param {Object} resources - Resources to load array
+	 * @param {string} prefix - URL path prefix to load the resources from
+	 * @author Eran Zinman
+	 */
+	me.loadResources = function(resources, prefix)
+	{
+		// Get resources to load
+		var resourcesToLoad = getResourcesToLoad(resources, prefix);
+		
+		// Loop through resources
+		for (var resourceIndex in resourcesToLoad)
+		{
+			var resource = resourcesToLoad[resourceIndex];
+			loadResource(resource.file, resource.type);	
+		}
+	};
+	
+	/**
+	 * Will make actual write to HTML, to load all the required
+	 * resources
+	 * 
+	 * @author Eran Zinman
+	 */
+	me.flush = function()
+	{
+		// Write document write buffer to document
+		document.write(documentWrite);
+		
+		// Clear document write buffer
+		documentWrite = '';
+	};
+    
+	return me;
+}());
+
+/**
+ * Self executing function which simply loads the resources array
+ * 
+ * @author Eran Zinman
+ */
+(function loadAllResources()
+{
+	if (window.__resources) 
+	{
+		ResourceLoader.loadResources(window.__resources);
+	}
+	
+	// Document write to HTML
+	ResourceLoader.flush();
+})();
+
+
